2d7cca286653471d4fa05207812d275dc0b47613
Vivian Lu <85651899+vivianxlu@users.noreply.github.com>
2024-09-26 21:02:59 +0000

    Fixed -Werror=sign-compare

--- src/hw2.c
+++ src/hw2.c
@@ -19,7 +19,7 @@ void print_packet(unsigned int packet[])
 
     printf("Packet Type: %s\n", ((packet[0] >> 30) & 0x3) == 1 ? "Write" : "Read");
     printf("Address: %u\n", packet[2] & 0xFFFFFFFF);
-    printf("Length: %u\n", packet[0] & 0x3FF);
+    printf("Length: %d\n", packet[0] & 0x3FF);
     printf("Requester ID: %u\n", (packet[1] >> 16) & 0xFFFF);
     printf("Tag: %u\n", (packet[1] >> 8) & 0xFF);
     printf("Last BE: %u\n", (packet[1] >> 4) & 0xF);
@@ -27,7 +27,7 @@ void print_packet(unsigned int packet[])
     
     if (((packet[0] >> 30) & 0x3) == 1) {
         printf("Data: ");
-        for (int i = 3; i < (packet[0] & 0x3FF) + 3; i++) {
+        for (unsigned int i = 3; i < (packet[0] & 0x3FF) + 3; i++) {
             printf("%u", packet[i] & 0xFFFFFFFF);
             if (i < (packet[0] & 0x3FF) + 2) {
                 printf(" ");
